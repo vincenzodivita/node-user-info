2fe69e030a233d0d9302d63359683825ab6e2868
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 23:02:52 +0200

    Add ram info

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const os = require("os");
 
 // Username info
 const info = os.userInfo().username;
+const totalRam = os.totalmem() / Math.pow(1024, 3);
+const freeRam = os.freemem() / Math.pow(1024, 3);
 
 // Print info
 console.log("Username: " + info);
+console.log("Total RAM: " + totalRam);
+console.log("Free RAM: " + freeRam);
